98e7cd6e12b8b3adf7e947b0067a49b65920877d
Bram Buurlage <brambuurlage@gmail.com>
2021-07-14 10:45:13 +0200

    work on cool macro

--- examples/counter.rs
+++ examples/counter.rs
@@ -11,6 +11,52 @@ enum Message {
     DownPressed,
 }
 
+macro_rules! ui {
+    { $w:expr $(=>{ $(
+        $(;for $x:ident in $i:expr =>)?
+        $(;if $(let $y_pat:pat =)? $y:expr =>)?
+        $child:expr $(=>$sub_child:tt)?
+        $(;else => $else_child:expr $(=>$else_sub_child:tt)?)?
+        $(;else if $(let $z_pat:pat =)? $z:expr => $else_if_child:expr $(=>$else_if_sub_child:tt)?)?
+    )* })? } => {
+        $w$($(.extend(ui!{ inner
+            $(;for $x in $i =>)?
+            $(;if $(let $y_pat =)? $y =>)?
+            $child $(=>$sub_child)?
+            $(;else =>)?
+            $(;else if $(let $z_pat =)? $z =>)?
+        }))*)?.into_node()
+    };
+
+    { inner ;for $x:ident in $i:expr => $w:expr $(=>$cs:tt)? } => {
+        $i.into_iter().map(|$x| ui!{$w $(=>$cs)?})
+    };
+    { inner ;if $(let $y_pat:pat =)? $y:expr => $w:expr $(=>$cs:tt)? } => {
+        if $(let $y_pat =)? $y {
+            Some(ui!{$w $(=>$cs)?})
+        } else {
+            None
+        }
+    };
+    { inner ;if $(let $y_pat:pat =)? $y:expr => $w:expr $(=>$cs:tt)? ;else => } => {
+        if $(let $y_pat =)? $y {
+            Some(ui!{$w $(=>$cs)?})
+        } else {
+            Some(ui!{$else_w $(=>$else_cs)?})
+        }
+    };
+    { inner ;if $(let $y_pat:pat =)? $y:expr => $w:expr $(=>$cs:tt)? ;else if $($z_pat:pat =)? $z:expr => $else_w:expr $(=>$else_cs:tt)? } => {
+        if $(let $y_pat =)? $y {
+            Some(ui!{$w $(=>$cs)?})
+        } else {
+            Some(ui!{$else_w $(=>$else_cs)?})
+        }
+    };
+    { inner $w:expr $(=>$cs:tt)? } => {
+        Some(ui!{$w $(=>$cs)?})
+    };
+}
+
 impl Component for Counter {
     type Message = Message;
     type State = i32;
@@ -21,11 +67,27 @@ impl Component for Counter {
     }
 
     fn view(&self, state: &i32) -> Node<Message> {
-        Column::new()
-            .push(Button::new(Text::new("Up")).on_clicked(Message::UpPressed))
-            .push(Text::new(format!("Count: {}", *state)))
-            .push(Button::new(Text::new("Down")).on_clicked(Message::DownPressed))
-            .into_node()
+        let mut test = Some("pattern");
+        test.take();
+        ui! {
+            Column::new() => {
+                Button::new("Up")
+                    .on_clicked(Message::UpPressed)
+
+                Column::new() => {
+                    ;for x in ["a", "b", "c"].iter() => x
+                    ;if let Some(x) = test => x
+                    ;else => "lolno"
+                    //:else if false => Row::new()
+                    //:else => "hoi"
+                }
+
+                format!("Count: {}", *state)
+
+                Button::new("Down")
+                    .on_clicked(Message::DownPressed)
+            }
+        }
     }
 
     fn update(
